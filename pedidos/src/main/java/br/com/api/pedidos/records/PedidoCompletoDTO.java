03307f67cd495896175ad68590fcfa3b93f292d8
viniciuslemes10 <vinikjhgfds@gmail.com>
2024-04-28 14:30:24 -0300

    Colocando exemplo no swagger.

--- pedidos/src/main/java/br/com/api/pedidos/records/PedidoCompletoDTO.java
+++ pedidos/src/main/java/br/com/api/pedidos/records/PedidoCompletoDTO.java
@@ -8,11 +8,11 @@ import java.util.List;
 import java.util.Map;
 
 public record PedidoCompletoDTO(
-        @Schema(description = "Mapa de pedidos", example = "{\"pedido1\": {\"caixa1\": [{\"sku\":\"pendrive\",\"qtd\":3}]}}")
+        @Schema(description = "Mapa de pedidos e suas caixas", example = "{\"pedido1\":{\"caixa1\":[{\"sku\":\"pendrive\",\"qtd\":3}],\"caixa2\":[{\"sku\":\"mouse\",\"qtd\":2},{\"sku\":\"pendrive\",\"qtd\":2}]},\"pedido2\":{\"caixa1\":[{\"sku\":\"pendrive\",\"qtd\":2},{\"sku\":\"mouse\",\"qtd\":2},{\"sku\":\"keyboard\",\"qtd\":2}]},\"pedido3\":{\"caixa1\":[{\"sku\":\"pendrive\",\"qtd\":2}],\"caixa2\":[{\"sku\":\"mouse\",\"qtd\":2}],\"caixa3\":[{\"sku\":\"monitor\",\"qtd\":1}]}}")
         @JsonProperty("pedidos")
         @NotNull
         Map<String, Map<String, List<ItemDTO>>> pedidos,
-        @Schema(description = "Mapa de peso por SKU", example = "{\"pendrive\": 50, \"mouse\": 210, \"keyboard\": 730, \"monitor\": 3100}")
+        @Schema(description = "Mapa de peso por SKU", example = "{\"pendrive\":50,\"mouse\":210,\"keyboard\":730,\"monitor\":3100}")
         @JsonProperty("pesoPorSku")
         @NotNull
         Map<String, Double> pesoPorSku
